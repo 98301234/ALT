75883895b9810fa05a408730db929614b5e6d903
98301234 <74495615+98301234@users.noreply.github.com>
2020-11-15 20:08:09 +0530

    Add files via upload

---
+++ brown.js
@@ -0,0 +1,65 @@
+function dash(){
+    window.alert=Date();
+}
+function hoe(){
+    document.getElementById("till").src="bulb on.png";
+}
+function tone(){
+    document.getElementById('till').src="bulb off.png";
+}
+function tiger(){
+    var x, y, z;
+    x = 5;      
+    y = 6;      
+    z = x + y+93;
+    document.getElementById("demo").innerHTML =
+"The value of z is " + z + "."; 
+}
+function snare(){
+var    x = 37;      
+var    y = 32;      
+var    z = x-y +10;
+    document.getElementById("demo").innerHTML =
+"The marks scored by triparn is" + z + ".";
+}
+function boy(){
+var price1 = 545168418149684189489498441488948941954988484884941554199449884198448419541988499;
+var price2 = 692917591579812791272198717997919101981299709792789029779829274218992044104912012;
+var total = price1 + price2;
+document.getElementById("demo").innerHTML =
+"The total is: " + total;
+}
+function low(){
+    var _co ="carbon monoxide";
+    var $ = 75;
+window.alert(_co);
+document.write(+$);
+}
+function long(){
+    var car={
+        type:"maruti",
+        size:"large",
+        weight:"2q"
+        };
+    window.alert("the weight of car is " +car.weight);
+}
+function fount(){
+    var bgs = ["A+ ","b+ ","o+ "];
+	bgs[0] ="A-";
+	bgs[1] ="hyper";
+	window.alert(bgs);
+	console.log("This ia a dome");
+	
+}
+function madh(){
+	document.getElementById('tome').style.display="none";
+}
+function qwerty(){
+	var japanese = ["Hiragana","Katakana","Kanji"];
+	var both = japanese[japanese.length-1];
+	window.alert(both);
+	document.getElementById("koya").innerHTML="japanese";
+
+}
+
+
---
+++ my.php
@@ -0,0 +1,10 @@
+<?php
+$txt = "hello";
+$te = 30;
+$hoe=90;
+echo 'my first php';
+echo "<br>";
+echo $txt;
+echo "<br>";
+echo "my favourite is $te/$hoe";
+?>
